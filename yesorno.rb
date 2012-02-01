256bb4fff757e3bf6fd57a44b52fa9dea6329896
Jesse Kriss <jesse@jklabs.net>
2012-02-01 07:53:55 -0600

    Adding time stamp

--- yesorno.rb
+++ yesorno.rb
@@ -6,16 +6,16 @@ require 'open-uri'
 
 def latest_tweet(screen_name)
   screen_name = screen_name[0,15]
-  begin
+  # begin
     json = open("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{screen_name}&count=1").read
     tweet = JSON.parse(json)
-    text = tweet.size > 0 ? tweet[0]['text'] : ''
-    @tweet_url = tweet.size > 0 ? "https://twitter.com/#{screen_name}/status/#{tweet[0]['id_str']}" : ''
-    puts "url: #{@tweet_url}"
+    text = tweet[0]['text']
+    @tweet_url = "https://twitter.com/#{screen_name}/status/#{tweet[0]['id_str']}"
+    @tweet_time = Time.parse(tweet[0]['created_at'])
     text
-  rescue
-    ''
-  end
+  # rescue
+  #   ''
+  # end
 end
 
 get '/' do
@@ -43,8 +43,10 @@ __END__
   %head
     %title #{@screen_name}?
     :css
+      * { margin: 0 ; padding: 0 }
+      .time { color: #aaa; font-size: 12px }
       body { margin-top: 80px; text-align:center; font-family: Helvetica, Arial, sans-serif }
-      h1 { font-size: 48pt }
+      h1 { margin-bottom: 10px; font-size: 48pt }
       h1, h1 a { color: #000; text-decoration: none }
   %body
     = yield
@@ -53,3 +55,4 @@ __END__
 %h1
   %a{ :href => @tweet_url }
     = @answer
+%div.time as of #{@tweet_time.strftime('%a, %b %-d %Y, %-I:%m %p')}
